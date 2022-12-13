7d71a95952cb4e2a4f5ec476b1d24d1e1e2bd697
Daniel Alves Farias <daniel_h.m@outlook.com>
2022-12-13 15:13:41 -0300

    Criado o Model e Controller de Login

---
+++ crud.api/Controllers/UsuarioController.cs
@@ -0,0 +1,17 @@
+﻿using crud.api.Models.Usuarios;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace crud.api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsuarioController : ControllerBase
+    {
+        [HttpPost]
+        public IActionResult Logar(LoginViewModelInput loginViewModelInput)
+        {
+            return Created("", loginViewModelInput);
+        }
+    }
+}
---
+++ crud.api/Models/Usuarios/LoginViewModelInput.cs
@@ -0,0 +1,8 @@
+﻿namespace crud.api.Models.Usuarios
+{
+    public class LoginViewModelInput
+    {
+        public string Login { get; set; }
+        public string Senha { get; set; }
+    }
+}
