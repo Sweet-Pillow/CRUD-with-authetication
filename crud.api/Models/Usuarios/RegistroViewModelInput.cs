40a969f001e1972eff68e49fba9dd9746a3adcb2
Daniel Alves Farias <daniel_h.m@outlook.com>
2022-12-14 10:46:44 -0300

    Criado Model e Controller de Registro

---
+++ crud.api/Models/Usuarios/RegistroViewModelInput.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace crud.api.Models.Usuarios
+{
+    public class RegistroViewModelInput
+    {
+        [Required(ErrorMessage = "O Login é obrigatório")]
+        public string Login { get; set; }
+
+        [Required(ErrorMessage = "O E-Mail é obrigatorio")]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "A senha é obrigatoria")]
+        public string Senha { get; set; }
+    }
+}
